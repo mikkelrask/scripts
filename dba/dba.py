65e9c0e83d86d978b189be591cfdeebb3d4a1b06
Mikkel Rask <mikkelrask@protonmail.com>
2021-02-09 17:59:09 +0100

    New script: dba.py
    As always a simple script. This one is a selenium scraper, that searches the danish craigslist wannabe Den Blå Avis for a set list of items, with min and max prices.

---
+++ dba/dba.py
@@ -0,0 +1,106 @@
+"""
+This script will open dba.dk in a headless Chrome browser, and search the
+products within a certain pricerange, saved in an excel sheet or CSV file.
+"""
+
+import time
+import pickle
+import csv
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+# Chrome settings
+chrome_options = Options()
+chrome_options.add_argument("--no-sandbox") # linux only
+#chrome_options.add_argument("--incognito")
+chrome_options.add_argument("--headless")
+
+#Initialize the browser and declare where to go
+driver = webdriver.Chrome(options=chrome_options)
+DBA = "https://dba.dk"
+GG = "https://guloggratis.dk"
+PRICE = 0
+MAXPRICE = 0
+PRODUCT = ""
+driver.implicitly_wait(5)
+
+def den_blaa_avis():
+    def remove(string): #remove spaces from PRODUCT to create individual .dat filename
+        return string.replace(" ","") 
+
+    # See if we have any data on the given product else db antal is 0
+    try:
+        db_antal = pickle.load(open(remove(PRODUCT) + ".dat", "rb"))
+    except:
+        db_antal = 0
+    driver.get(DBA)
+    time.sleep(2)
+    search = driver.find_element_by_id("searchField")
+    print("-------------------------------")
+
+    print("Produkt: " + PRODUCT)
+    search.send_keys(PRODUCT) # send search term / product name
+    search.send_keys(Keys.RETURN)
+    time.sleep(2)
+
+    driver.find_element_by_xpath("//h4[contains(text(), 'Pris')]").click() # Set our price wishes
+    print("Pris: " + str(PRICE)+"-"+str(MAXPRICE))
+    price = driver.find_element_by_class_name("rangeFrom") # Min
+    price.send_keys(PRICE)
+    price = driver.find_element_by_class_name("rangeTo") # Max
+    price.send_keys(MAXPRICE)
+    price.send_keys(Keys.RETURN)
+    time.sleep(2)
+
+    try:
+        #if driver.find_element_by_xpath("//td[contains(text(),'annoncer')]"):
+        antal_annoncer_string = driver.find_element_by_xpath("//td[contains(text(),\
+                    'annoncer')]").get_attribute("innerHTML").strip()
+        antal = [int(i) for i in antal_annoncer_string.split() if i.isdigit()] # Get only the integer
+        diff = int(antal[0]) - db_antal
+        if diff > 0:
+            print(str(antal[0]) + ' annoncer fundet. ' + str(diff) + '+ ift forrige søgning.')
+            print("URL: " + driver.current_url)
+            print("-------------------------------")
+            pickle.dump(int(antal[0]), open(remove(PRODUCT) + ".dat", "wb")) # Dump the new number of items into the database
+        elif diff == 0:
+            print ("Ingen nye annoncer.")
+            print("-------------------------------")
+        else:
+            print(str(diff) + " ift. sidst søgning")
+            pickle.dump(int(antal[0]), open(remove(PRODUCT) + ".dat", "wb")) # Dump the new number of items into the database
+    except:
+        print(PRODUCT + " ikke fundet i prisklassen.")
+        print("-------------------------------")
+
+# Check the same on GulGratis
+def gul_og_gratis():
+    driver.get(GG)
+    time.sleep(2)
+    search = driver.find_element_by_id("searchField")
+    search.send_keys(PRODUCT)
+    search.send_keys(Keys.RETURN)
+    time.sleep(2)
+    driver.find_element_by_xpath("//h4[contains(text(), 'Pris')]").click()
+    print("Searching...")
+    price = driver.find_element_by_class_name("rangeFrom")
+    price.send_keys(PRICE)
+    price = driver.find_element_by_class_name("rangeTo")
+    price.send_keys(MAXPRICE)
+    price.send_keys(Keys.RETURN)
+    time.sleep(2)
+    print("Produkt: " + PRODUCT)
+    print(driver.find_element_by_xpath("//td[contains(text(),'annoncer')]").get_attribute("innerHTML").strip() + " i prisklassen " + str(PRICE) + " - " + str(MAXPRICE))
+    print("URL:" + driver.current_url)
+
+filename = "search_agent.csv"
+
+print("Søger...")
+with open(filename, "r") as csvfile:
+    datareader = csv.reader(csvfile)
+    for row in datareader:
+        PRODUCT = row[0]
+        PRICE = row[1]
+        MAXPRICE = row[2]
+        den_blaa_avis()
